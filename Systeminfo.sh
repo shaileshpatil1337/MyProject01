0ae13f5231aa851acb7482d4a1d79f29f0c315e7
shailesh.patil1337 <shailesh.patil1337@gmail.com>
2024-01-02 14:16:15 +0000

    New Changes

--- Systeminfo.sh
+++ Systeminfo.sh
@@ -11,13 +11,20 @@ ipv4_enp0s3=$(ip addr show enp0s3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | xargs)
 ipv4_enp0s8=$(ip addr show enp0s8 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | xargs)
 
 # Get current date and time
-current_datetime=$(date)
+current_datetime=$(date +"%Y%m%d_%H%M%S")
 
-# Print the formatted output
-echo "This is Test Script"
-echo "System information as of $current_datetime"
-echo ""
-echo "  System load:  $system_load               Processes:               $process_count"
-echo "  Usage of /:   $disk_usage% of $(df -h / | awk 'NR==2 {print $2}')   Users logged in:         $users_logged_in"
-echo "  Memory usage: $memory_usage%                 IPv4 address for enp0s3: $ipv4_enp0s3"
-echo "  Swap usage:   $swap_usage%                 IPv4 address for enp0s8: $ipv4_enp0s8"
+# Formatted output
+output="System information as of $current_datetime\n\n"
+output+="  System load:  $system_load               Processes:               $process_count\n"
+output+="  Usage of /:   $disk_usage% of $(df -h / | awk 'NR==2 {print $2}')   Users logged in:         $users_logged_in\n"
+output+="  Memory usage: $memory_usage%                 IPv4 address for enp0s3: $ipv4_enp0s3\n"
+output+="  Swap usage:   $swap_usage%                 IPv4 address for enp0s8: $ipv4_enp0s8\n"
+
+# Print formatted output
+echo -e "$output"
+
+# Save to a file with date and time
+output_file="system_info_$current_datetime.txt"
+echo -e "$output" > "$output_file"
+echo "System information has been saved to $output_file"
+
